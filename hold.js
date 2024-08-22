842383770818744c77f986bcdf335ccefc9f8770
Johny-Dev5 <167773963+Johny-Dev5@users.noreply.github.com>
2024-08-22 14:17:17 +0700

    Delete hold.js

--- hold.js
+++
@@ -1,5 +0,0 @@
- let products = JSON.parse(localStorage.getItem('products')) || [
-                { name: 'Product 1', price: 100 },
-                { name: 'Product 2', price: 200 }
-            ];
-
