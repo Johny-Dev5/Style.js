0bd2f5ded4e88762d2de5181a66d451a4d383eeb
Johny-Dev5 <167773963+Johny-Dev5@users.noreply.github.com>
2025-04-27 21:42:33 +0700

    Update PPPP.js

--- PPPP.js
+++ PPPP.js
@@ -1,15 +1,6 @@
-const productUrl = "https://script.google.com/macros/s/AKfycbw6Fb1_54XrPWaqMN-WAS9tO0uhoElzY-NJnkOlrpEAmLEwn2nGgBYSTEbEQTwg9xMU_w/exec";
-
-async function fetchProducts() {
-  try {
-    const response = await fetch(productUrl);
-    const products = await response.json();
-    console.log(products); // ✅ Products will appear here
-    return products;
-  } catch (error) {
-    console.error("❌ Failed to fetch products:", error);
-    return [];
-  }
+// Product.js
+export async function fetchProducts() {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbw6Fb1_54XrPWaqMN-WAS9tO0uhoElzY-NJnkOlrpEAmLEwn2nGgBYSTEbEQTwg9xMU_w/exec');
+  const data = await response.json();
+  return data;
 }
-
-export { fetchProducts };
