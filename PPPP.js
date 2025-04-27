92473ed3de7cca5bf9caeb65ce6a3109bcdac20e
Johny-Dev5 <167773963+Johny-Dev5@users.noreply.github.com>
2025-04-27 21:23:52 +0700

    PPPP.js

--- PPPP.js
+++ PPPP.js
@@ -1,15 +1,216 @@
-const productUrl = "https://script.google.com/macros/s/AKfycbw6Fb1_54XrPWaqMN-WAS9tO0uhoElzY-NJnkOlrpEAmLEwn2nGgBYSTEbEQTwg9xMU_w/exec";
 
-async function fetchProducts() {
-  try {
-    const response = await fetch(productUrl);
-    const products = await response.json();
-    console.log(products); // ✅ Products will appear here
-    return products;
-  } catch (error) {
-    console.error("❌ Failed to fetch products:", error);
-    return [];
-  }
-}
+const products = [
+   {
+        id: 1,
+        name: 'ផ្កាថូចាក់អំបោះ',
+        images: [
+            'https://drive.google.com/thumbnail?id=1HHZLv7asjMGK0Ta1-WaFM65MfXEXair-&sz=w1000-h1000'
+        ],
+        colors: [
+            {
+                name: 'ផ្កា Daisy​ចំរុះពណ៌',
+                price: 2,
+                image: 'https://drive.google.com/thumbnail?id=1TehiEx0M2EsA7ezbDV-o0vaodWoiweIX&sz=w1000-h1000',
+                stock: true,
+            },
+            {
+                name: 'ផ្កា Tulip',
+                price: 3,
+                image: 'https://drive.google.com/thumbnail?id=1ylICfzwSjaJDWDjnBsDw6rLsu5kBHnnw&sz=w1000-h1000',
+                stock: true,
+               
+            },
+            {
+                name: 'ផ្កា កុលាប Mini',
+                price: 2,
+                image: 'https://drive.google.com/thumbnail?id=1W4Pz-JSyb4dUQlK_G2JtZANRN-cHL79A&sz=w1000-h1000',
+                stock: true,
+            },
+             {
+                name: 'ផ្កាឈូករ័ត្នរីករាយ',
+                price: 2,
+                image: 'https://drive.google.com/thumbnail?id=1iVuU2Ko5nqggF8M2mx4KtBcqnKchHIRX&sz=w1000-h1000',
+                stock: true,
+            },
+             {
+                name: 'ផ្កាកុលាបលឿង',
+                price: 2,
+                image: 'https://drive.google.com/thumbnail?id=1TWse1fOMTP2bCM7lZLlNaLU_TngMHMCr&sz=w1000-h1000',
+                stock: true,
+            },
+            {
+                name: 'ផ្កាកុលាបក្រហម',
+                price: 2,
+                image: 'https://drive.google.com/thumbnail?id=1Bv5EwT8ur2P3V5BejYt52ZPnmV-Ovgxu&sz=w1000-h10000',
+                stock: true,
+            },
+                       {
+                name: 'ផ្កាឈូករ័ត្នតុក្កតា ',
+                price: 2,
+                image: 'https://drive.google.com/thumbnail?id=14nMPAu7H1XNuCsrxYIElicrUCCorBFbs&sz=w1000-h1000',
+                stock: true,
+            },
+                       {
+                name: 'ម៉ាសវេទមន្តផ្កាស្មៅ​កំប៉ុង​ធំ​ ',
+                price: 2,
+                image: 'https://drive.google.com/thumbnail?id=1A1y8_kOq35zhZF76EUcOP4BsfS_WZslt&sz=w1000-h1000',
+                stock: true,
+            }
+        ],
+        
+        stock: true,
+        category: 'ថូផ្កា',
+        description: '<p>ផ្កាថូរដាក់តាំងលំអ លើតុ ក្នុងឡាន កន្លែងធ្វើការ ទិញជូនមនុស្សជាទីស្រលាញ់ជាពិសេស</p>',
+    },
+    {
+        id: 2,
+        name: 'ផ្លិតម៉ូតចិន',
+        images: [
+            'https://i.pinimg.com/736x/c8/2a/68/c82a68022171b1b5d8336cab10ca9693.jpg'
+        ],
+        colors: [
+            {
+                name: '01',
+                price: 2.5,
+                image: 'https://i.pinimg.com/736x/c8/2a/68/c82a68022171b1b5d8336cab10ca9693.jpg',
+                stock: true,
+            },
+            {
+                name: '02',
+                price: 2.5,
+                image: 'https://i.pinimg.com/736x/87/51/ce/8751ce09dea3f0482414c9659d8a2b9f.jpg',
+                stock: true,
+               
+            },
+            {
+                name: '03',
+                price: 2.5,
+                image: 'https://i.pinimg.com/736x/6c/85/5d/6c855d8e07668574ffdd164052846fd5.jpg',
+                stock: true,
+            },
+             {
+                name: '04',
+                price: 2.5,
+                image: 'https://i.pinimg.com/736x/5f/d5/10/5fd5108c4fdedef644d25f8afc2ff7f2.jpg',
+                stock: true,
+            },
+             {
+                name: '05',
+                price: 2.5,
+                image: 'https://i.pinimg.com/736x/93/28/57/932857e6245ed958b6c93bd80484c612.jpg',
+                stock: true,
+            }
+        ],
+        stock: true,
+        category: 'ផ្លិតម៉ូតចិន',
+        description: '<p></p>',
+    },
+    
+    {
+        id: 3,
+        name: 'ខ្សែដៃ ពេញនិម',
+        images: [
+            'https://i.pinimg.com/736x/13/a7/7e/13a77ec51b068a5efcee0436900a532f.jpg'
+        ],
+        colors: [
+            {
+                name: '01',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/13/a7/7e/13a77ec51b068a5efcee0436900a532f.jpg',
+                stock: true,
+            },
+            {
+                name: '02',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/f9/bc/e4/f9bce416fe51a26d5f638309a9065f5c.jpg',
+                stock: true,
+               
+            },
+            {
+                name: '03',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/81/5f/ff/815fffa2168dad4637075ec07ca12fc5.jpg',
+                stock: true,
+            },
+             {
+                name: '04',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/fa/b1/80/fab1805686a8017690a2b07c13da7207.jpg',
+                stock: true,
+            },
+             {
+                name: '05',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/09/91/bf/0991bf1b0944f693c778caa1a8e6c336.jpg',
+                stock: true,
+            },
+            {
+                name: '06',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/e6/44/ce/e644ce4b0aad6f411e24e9424f126af6.jpg',
+                stock: true,
+            },
+             {
+                name: '07',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/ce/8b/da/ce8bda4716e1902855417a6eecea13ce.jpg',
+                stock: true,
+            },
+            {
+                name: '08',
+                price: 1,
+                image: 'https://i.pinimg.com/736x/6b/87/6c/6b876c95cfdefd624ea862d9a4348e82.jpg',
+                stock: true,
+            }
+        ],
+        stock: true,
+        category: 'ខ្សែដៃ',
+        customText: 'សម្រាប់ការកម្មងបន្ថែមអាចឆាតមក FB Page បានបង',
+        description: '<p>ខ្សែដៃតម្លៃ 4000រៀល ពុំបានបូកបញ្ចូលជាមួយអប់​សម្រាប់ប្រអប់បងអាចទិញប្រអប់បានបង</p>',
+    },
+{
+        id: 4,
+        name: 'បន្តោង Cute Cute',
+        images: [
+            'https://i.pinimg.com/736x/15/1a/48/151a489f34879414152ca46973cba6e0.jpg'
+        ],
+        colors: [
+            {
+                name: 'បន្តោង បូរ',
+              price: 0.25,
+                image: 'https://i.pinimg.com/736x/15/1a/48/151a489f34879414152ca46973cba6e0.jpg',
+                stock: true,
+            },
+            {
+                name: 'បន្តោង ផ្កា',
+                price: 0.25,
+                image: 'https://i.pinimg.com/736x/fe/f2/c7/fef2c77482281bc459505e050f648ec6.jpg',
+                stock: true,
+               
+            },
+            {
+                name: 'បន្តោង ល្ពៅ',
+               price: 0.25,
+                image: 'https://i.pinimg.com/736x/68/be/a4/68bea45a48f6fd7be36e140d5c47e546.jpg',
+                stock: true,
+            },
+             {
+                name: 'បន្តោង Cherry',
+              price: 0.25,
+                image: 'https://i.pinimg.com/736x/37/1a/ef/371aef8c14ed8e6ef79b90870958a0fc.jpg',
+                stock: true,
+            },
+             {
+                name: 'បន្តោង មេអំបៅ',
+                price: 0.25,
+                image: 'https://i.pinimg.com/736x/8a/38/75/8a38751d44f81f5b3b3bef28f242476f.jpg',
+                stock: true,
+            }
+        ],
+        stock: true,
+        category: '1000រៀល',
+        tag: 'Best Selling',
+        description: '<p></p>',
+    },
 
-export { fetchProducts };
+];
