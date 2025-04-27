c3ea64575a487cc04f1928682489633a67c31f77
Johny-Dev5 <167773963+Johny-Dev5@users.noreply.github.com>
2025-04-27 21:43:29 +0700

    PPPP.js

--- PPPP.js
+++ PPPP.js
@@ -1,6 +1,17 @@
-// Product.js
-export async function fetchProducts() {
-  const response = await fetch('https://script.google.com/macros/s/AKfycbw6Fb1_54XrPWaqMN-WAS9tO0uhoElzY-NJnkOlrpEAmLEwn2nGgBYSTEbEQTwg9xMU_w/exec');
-  const data = await response.json();
-  return data;
-}
+const products = 
+[
+  {
+    "id": 1,
+    "name": "មួក",
+    "preOrder": false,
+    "stock": true,
+    "category": "Clothing",
+    "productImageUrls": [
+      ""
+    ],
+    "description": "ថ្មី",
+    "price": 2,
+    "compare": "",
+    "discount": ""
+  }
+]
